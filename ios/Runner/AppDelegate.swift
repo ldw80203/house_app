cd13e56812b20b564a7305e4f6969b28c75b81da
ldw80203 <79986154+ldw80203@users.noreply.github.com>
2026-01-18 01:52:34 +0800

    Update AppDelegate.swift
    修復 iOS 閃退：新增 plist 權限與 AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,15 @@
-import Flutter
 import UIKit
-import GoogleMaps
+import Flutter
+import GoogleMaps // 1. 引入 Google Maps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR_API_KEY")
+    // 2. 在這裡註冊你的 API Key (非常重要，沒這行會閃退)
+    GMSServices.provideAPIKey("AIzaSyCD7BJq7y3CAYnv2w6dBCVLGMuzQp-gxTwPS_API_KEY") 
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
